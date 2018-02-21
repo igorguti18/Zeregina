33ceddae9d1f1096213b092c2a3283a86519762f
igorguti18 <sasiroot@NA106-US05.ikaira.iurretalhi.eus>
2018-02-21 08:59:39 +0100

    Add third button

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -28,6 +28,7 @@ public class MainFrame extends javax.swing.JFrame {
 
         jButton1 = new javax.swing.JButton();
         jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new java.awt.GridLayout());
@@ -47,6 +48,9 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
         getContentPane().add(jButton2);
+
+        jButton3.setText("Button3");
+        getContentPane().add(jButton3);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -99,5 +103,6 @@ public class MainFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     // End of variables declaration//GEN-END:variables
 }
